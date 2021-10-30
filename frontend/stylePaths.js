1ac68462b462e65ca838c31beabb2201b05dc2e9
Sharif Md. Minhazur Rahman Rabbi <68814275+Minhaz540@users.noreply.github.com>
2021-10-31 00:19:40 +0600

    practice

--- frontend/stylePaths.js
+++ frontend/stylePaths.js
@@ -6,7 +6,7 @@ module.exports = {
     path.resolve(__dirname, 'node_modules/@patternfly/patternfly'),
     path.resolve(__dirname, 'node_modules/@patternfly/react-styles/css'),
     path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/styles/base.css'),
-    path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/esm/@patternfly/patternfly'),
+    path.resolve(__dirname, 'node_/@patternfly/react-core/dist/esm/@patternfly/patternfly'),
     path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css'),
     path.resolve(__dirname, 'node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css'),
     path.resolve(__dirname, 'node_modules/@patternfly/react-inline-edit-extension/node_modules/@patternfly/react-styles/css')
