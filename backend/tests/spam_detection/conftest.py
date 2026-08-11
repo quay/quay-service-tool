9a07f80e92ed9afe8f7b6fa798fe738ac470fdda
Jordi Piriz <jpiriz@redhat.com>
2026-08-11 08:51:45 +0200

    Revert "QUAYIO-1637: add spam detection service-tool workflow (#216)"
    This reverts commit 3f8534ef9f9f13d88dcb1ec408e6822de688f4e7.

--- backend/tests/spam_detection/conftest.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-
-BACKEND_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
-if BACKEND_ROOT not in sys.path:
-    sys.path.insert(0, BACKEND_ROOT)
